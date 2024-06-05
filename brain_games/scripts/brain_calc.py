51ba63c6fb238dabee702c9035a0c50515963557
Nikita Gorynin <nikita12341@mail.ru>
2024-06-05 13:48:06 +0300

    modifi brain-even and brain-calc scripts

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 from brain_games.games.engine import welcome_user, run_game
-from brain_games.games.brain_even import even_game, game_rule
+from brain_games.games.brain_calc import game_calc, game_rule
 
 
 def main():
     name = welcome_user()
     print(game_rule)
-    run_game(even_game, name)
+    run_game(game_calc, name)
 
 
 if __name__ == '__main__':
