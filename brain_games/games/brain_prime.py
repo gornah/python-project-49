65b7f7ba93a67413fcc3f7307361b507dfa31ee0
Nikita Gorynin <nikita12341@mail.ru>
2024-06-09 17:57:58 +0300

    add brain-prime game

---
+++ brain_games/games/brain_prime.py
@@ -0,0 +1,21 @@
+from math import sqrt
+from random import randint
+
+
+def is_prime(num):
+    count = 2
+    while count <= sqrt(num):
+        if num % count == 0:
+            return False
+        count += 1
+    return True
+
+
+def game_prime():
+    num = randint(2, 21)
+    question = (f'Question: {num}')
+    correct_answer = 'yes' if is_prime(num) else 'no'
+    return question, correct_answer
+
+
+game_rule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from brain_games.games.engine import welcome_user, run_game
+from brain_games.games.brain_prime import game_prime, game_rule
+
+
+def main():
+    name = welcome_user()
+    print(game_rule)
+    run_game(game_prime, name)
+
+
+if __name__ == '__main__':
+    main()
