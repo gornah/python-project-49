67687c621a3f317a95caa45e50a8ed543c668b5a
Nikita Gorynin <nikita12341@mail.ru>
2024-06-11 16:08:08 +0300

    project rev#9, upd game even, add is_even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,11 +1,18 @@
 from random import randint
 
 
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+
 # even game
 def game():
     number = randint(1, 100)
     question = (f'Question: {number}')
-    correct_answer = 'yes' if number % 2 == 0 else 'no'
+    correct_answer = 'yes' if is_even(number) else 'no'
     return question, correct_answer
 
 
