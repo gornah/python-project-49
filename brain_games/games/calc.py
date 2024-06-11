e7f20da2555944ee89e71f022faf2260fcd75710
Nikita Gorynin <nikita12341@mail.ru>
2024-06-11 15:57:02 +0300

    project rev#7, eval change to dict in game calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,13 +2,32 @@ from random import randint
 from random import choice
 
 
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+}
+
+
 # calc game
 def game():
     x = randint(5, 10)
     y = randint(1, 5)
     operation = choice(['+', '-', '*'])
     question = (f'Question: {x} {operation} {y}')
-    correct_answer = eval(f'{x} {operation} {y}')
+    correct_answer = operations[operation](x, y)
     return question, correct_answer
 
 
